265d513bcf75498745d35feb9e7934b06da2d304
imtiaz782 <101878458+imtiaz782@users.noreply.github.com>
2022-04-02 16:33:32 +0500

    Binding&List
    lesson 5

---
+++ Binding&List.R
@@ -0,0 +1,23 @@
+# cbind-ing and rbind-ing
+# Matrices can be created by column-binding or ROW-BINDING WITH CBIND () and rbind().
+x <- 1:3
+y <- 10:12
+
+cbind(x,y) # here we giving the column Name (x,Y)
+
+rbind(x,y) # here we giving the Row Name (x,Y)
+
+a <- 20:25
+b <- 25:30
+
+cbind(a,b)
+ 
+rbind(a,b)
+
+c = 4:8
+d = 10:14
+
+cbind(c,d)
+
+rbind(c,d)
+
